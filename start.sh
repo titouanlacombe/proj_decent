171e22cd213cf97d4099119176c1fe01c1952451
Titouan <titouan.lacombe99@gmail.com>
2022-12-07 16:33:56 +0100

    updated start

--- start.sh
+++ start.sh
@@ -2,12 +2,13 @@
 set -e
 
 export NUM_NODES=$1
+export ROOM_CAPACITY=$2
 
 # Build
 javac -d bin -cp bin src/**/*.java src/*.java
 
 # Start manager in background
-java -cp bin Manager $NUM_NODES &
+java -cp bin Manager $NUM_NODES $ROOM_CAPACITY &
 
 # Recover manager address
 sleep 0.5
