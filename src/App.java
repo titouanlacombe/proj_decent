8cfeed5aa58f662eda7c1230790422028e0f6940
Titouan <titouan.lacombe99@gmail.com>
2023-01-13 09:55:43 +0100

    added demos

--- src/App.java
+++ src/App.java
@@ -6,30 +6,29 @@ import javax.swing.*;
 import java.awt.Color;
 
 import config.Config;
-import sim.Simulator;
 
 public class App {
-    public void _main(String[] args) throws Exception {
-        Logging.init("App", "./data/app.log");
+	public void _main(String[] args) throws Exception {
+		Logging.init("App", "./data/app.log");
 
-        Config config = Config._default();
+		Config config = Config.demo1();
 
-        JFrame jframe = new JFrame(config.windowTitle);
-        jframe.setSize(config.windowWidth, config.windowHeight);
-        jframe.getContentPane().setBackground(Color.DARK_GRAY);
+		JFrame jframe = new JFrame(config.windowTitle);
+		jframe.setSize(config.windowWidth, config.windowHeight);
+		jframe.getContentPane().setBackground(Color.DARK_GRAY);
 
-        MainWindow mwindow = new MainWindow(Logger.fileLogger("UI", "./data/ui.log"), jframe, config);
-        mwindow.initWindow();
-        mwindow.configWindow();
-    }
+		MainWindow mwindow = new MainWindow(Logger.fileLogger("UI", "./data/ui.log"), jframe, config);
+		mwindow.initWindow();
+		mwindow.configWindow();
+	}
 
-    public static void main(String[] args) {
-        App app = new App();
+	public static void main(String[] args) {
+		App app = new App();
 
-        try {
-            app._main(args);
-        } catch (Exception e) {
-            Logging.exception(e);
-        }
-    }
+		try {
+			app._main(args);
+		} catch (Exception e) {
+			Logging.exception(e);
+		}
+	}
 }
--- src/config/Config.java
+++ src/config/Config.java
@@ -4,86 +4,111 @@ import java.lang.reflect.Field;
 import java.util.ArrayList;
 
 public class Config {
-    // --- Simulation config ---
-    public double entryRate;
-    public double visitTimeMean;
-    public double visitTimeStdDev;
-    public int roomCapacity;
-    public int numNodes;
-    public double timeScale;
-    public long randSeed;
-    public double simulationTimeFactor;
-    public long simulationUpdateInterval;
-    public long nodeSleepTime;
+	// --- Simulation config ---
+	public double entryRate;
+	public double visitTimeMean;
+	public double visitTimeStdDev;
+	public int roomCapacity;
+	public int numNodes;
+	public double timeScale;
+	public long randSeed;
+	public double simulationTimeFactor;
+	public long simulationUpdateInterval;
+	public long nodeSleepTime;
 
-    // --- UI config ---
-    public String windowTitle;
-    public int windowWidth;
-    public int windowHeight;
-    public long uiRefreshInterval;
+	// --- UI config ---
+	public String windowTitle;
+	public int windowWidth;
+	public int windowHeight;
+	public long uiRefreshInterval;
 
-    static public Config _default() {
-        Config config = new Config();
+	public ArrayList<String> getKeys() {
 
-        // In milliseconds
-        config.entryRate = 10 / 2000.0; // 1 entry every 2 seconds
-        config.visitTimeMean = 10000; // 1 second
-        config.visitTimeStdDev = 1000; // 1 second
+		ArrayList<String> keys = new ArrayList<>();
+		Field[] fields = getFields();
+		for (int i = 0; i < fields.length; i++) {
+			if (!fields[i].getName().equals("windowTitle") && !fields[i].getName().equals("windowWidth")
+					&& !fields[i].getName().equals("windowHeight"))
+				keys.add(fields[i].getName());
+		}
+		return keys;
+	}
 
-        config.roomCapacity = 10;
-        config.numNodes = 3;
-        config.timeScale = 1;
-        config.randSeed = 0;
-        config.simulationTimeFactor = 1;
-        config.simulationUpdateInterval = 20;
-        config.nodeSleepTime = 100;
+	public Field getField(String key) throws NoSuchFieldException {
+		return this.getClass().getDeclaredField(key);
+	}
 
-        config.windowTitle = "Simulation";
-        config.windowWidth = 800;
-        config.windowHeight = 600;
-        config.uiRefreshInterval = 20;
+	public Field[] getFields() {
+		return this.getClass().getDeclaredFields();
+	}
 
-        return config;
-    }
+	public Object get(String key) throws NoSuchFieldException, IllegalAccessException {
+		return this.getClass().getDeclaredField(key).get(this);
+	}
 
-    public ArrayList<String> getKeys() {
+	public void set(String key, Object value) throws NoSuchFieldException, IllegalAccessException {
+		this.getClass().getDeclaredField(key).set(this, value);
+	}
 
-        ArrayList<String> keys = new ArrayList<>();
-        Field[] fields = getFields();
-        for (int i = 0; i < fields.length; i++) {
-            if (!fields[i].getName().equals("windowTitle") && !fields[i].getName().equals("windowWidth")
-                    && !fields[i].getName().equals("windowHeight"))
-                keys.add(fields[i].getName());
-        }
-        return keys;
-    }
+	public String toString() {
+		// Using reflexion
+		String str = "Config:\n";
+		for (String key : this.getKeys()) {
+			try {
+				str += "\t" + key + ": " + this.get(key) + "\n";
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		return str;
+	}
 
-    public Field getField(String key) throws NoSuchFieldException {
-        return this.getClass().getDeclaredField(key);
-    }
+	static public Config _default() {
+		Config config = new Config();
 
-    public Field[] getFields() {
-        return this.getClass().getDeclaredFields();
-    }
+		// In milliseconds
+		config.entryRate = 10 / 2000.0;
+		config.visitTimeMean = 10000;
+		config.visitTimeStdDev = 1000;
 
-    public Object get(String key) throws NoSuchFieldException, IllegalAccessException {
-        return this.getClass().getDeclaredField(key).get(this);
-    }
+		config.roomCapacity = 10;
+		config.numNodes = 3;
+		config.timeScale = 1;
+		config.randSeed = 0;
+		config.simulationTimeFactor = 1;
+		config.simulationUpdateInterval = 20;
+		config.nodeSleepTime = 100;
 
-    public void set(String key, Object value) throws NoSuchFieldException, IllegalAccessException {
-        this.getClass().getDeclaredField(key).set(this, value);
-    }
+		config.windowTitle = "Museum Access Control - Monitoring";
+		config.windowWidth = 800;
+		config.windowHeight = 600;
+		config.uiRefreshInterval = 20;
 
-    public String toString() {
-        // Using reflexion
-        String str = "Config:\n";
-        for (String key : this.getKeys()) {
-            try {
-                str += "\t" + key + ": " + this.get(key) + "\n";
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-        return str;
-    }
+		return config;
+	}
+
+	static public Config demo1() {
+		Config config = _default();
+		config.roomCapacity = 50;
+		config.numNodes = 5;
+		config.nodeSleepTime = 1000;
+		return config;
+	}
+
+	static public Config demo2() {
+		Config config = demo1();
+		config.roomCapacity = 1000;
+		config.numNodes = 20;
+		config.nodeSleepTime = 5;
+		config.simulationUpdateInterval = 5;
+		config.uiRefreshInterval = 5;
+		config.entryRate = 100 / 2000.0;
+		return config;
+	}
+
+	static public Config demo3() {
+		Config config = demo2();
+		config.visitTimeMean *= 2.1;
+		return config;
+	}
 }
